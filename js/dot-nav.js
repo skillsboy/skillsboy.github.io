2d9449860cf17a6903ad8d667904bdc23bc77ac1
skillsboy <milesmontgomery4@gmail.com>
2021-09-18 17:51:12 +0100

    new navbar responsive

---
+++ js/dot-nav.js
@@ -0,0 +1,55 @@
+{
+    let intro = document.querySelector("#intro");
+    let about = document.querySelector("#about");
+    let projects = document.querySelector("#projects");
+    let contact = document.querySelector("#contact");
+    let dot0 = document.getElementById("dot-0");
+    let dot1 = document.getElementById("dot-1");
+    let dot2 = document.getElementById("dot-2");
+    let dot3 = document.getElementById("dot-3");
+    let sideDotNavLinks = document.querySelectorAll(".side-dot-nav>a");
+
+    window.addEventListener('resize', resize);
+
+    resize();
+
+
+    function setActiveDot() {
+        if (isInView(contact)) {
+            removeActiveDots();
+            dot3.classList.add("active");
+        } else if (isInView(projects)) {
+            removeActiveDots();
+            dot2.classList.add("active");
+        } else if (isInView(about)) {
+            removeActiveDots();
+            dot1.classList.add("active");
+        } else if (isInView(intro)) {
+            removeActiveDots();
+            dot0.classList.add("active");
+        }
+    }
+
+    function removeActiveDots() {
+        sideDotNavLinks.forEach(el => {
+            el.classList.remove("active");
+        });
+    }
+
+    function isInView(el) {
+        const box = el.getBoundingClientRect();
+        return box.top < window.innerHeight && box.bottom >= 0;
+    }
+
+    function resize() {
+        if (window.innerWidth >= 768) {
+            window.onscroll = function () {
+                setActiveDot();
+            };
+
+            setActiveDot();
+            return;
+        }
+        window.onscroll = "";
+    }
+}
